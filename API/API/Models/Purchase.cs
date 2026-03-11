e0cd6e7e357cfdb90cdf67445f797af10259f936
T0my75 <tomassegurado07@icloud.com>
2026-03-11 10:06:33 +0000

    classes iniciais do model

---
+++ API/API/Models/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using Mono.TextTemplating;
+
+namespace API.Models
+{
+    public class Purchase{
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public State State { get; set; }
+
+    }
+    public enum State{
+        Pending,
+        Paid,
+        Sent,
+        Delivered,
+        Closed
+    }
+}
